f3abfa9e4ce8ffabc44d5725ff4e66adc3a420d5
Tiramitzu <github@tira.my.id>
2023-11-12 19:22:17 +0700

    chore(components): remove lazy loading (not working)

--- src/components/loading.tsx
+++
@@ -1,7 +0,0 @@
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css'
-
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <Skeleton />
-  }
