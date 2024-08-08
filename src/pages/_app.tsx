e6cf698315fadc0e25c0b7a80901932d389bf38a
Syah Warid Ghani Akram <github@tira.my.id>
2024-08-08 18:39:09 +0700

    fix: @media queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-import "tailwindcss/tailwind.css";
 import "styles/styles.scss";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
