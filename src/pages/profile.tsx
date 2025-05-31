36cc1b62d5d8aec30ff4ece75ca48b95f02bea27
Syah Warid Ghani Akram <github@tira.my.id>
2025-05-31 19:44:58 +0700

    chore: disable something

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -7,7 +7,7 @@ import { TbCake, TbDownload, TbMail, TbGlobe, TbBrandLinkedin, TbPhone } from "r
 
 const Profile = ({ user, error }: { user: Discord.APIUser; error: string | null }) => {
     const [data, setData] = useState<number | null>(null);
-    const [status, setStatus] = useState<"online" | "idle" | "dnd" | "offline" | "invisible" | null>(null);
+    // const [status, setStatus] = useState<"online" | "idle" | "dnd" | "offline" | "invisible" | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,43 +24,44 @@ const Profile = ({ user, error }: { user: Discord.APIUser; error: string | null 
         });
     }, []);
 
-    useEffect(() => {
-        // Fetch Discord status every 10 seconds
-        const fetchStatus = () => {
-            fetch("/api/discord-status")
-                .then(res => res.json())
-                .then(data => setStatus(data.status))
-                .catch(() => setStatus(null));
-        };
-        fetchStatus();
-        const interval = setInterval(fetchStatus, 10000);
-        return () => clearInterval(interval);
-    }, []);
+    // FIXME: DISABLED FOR AN OBVIOUS REASON
+    // useEffect(() => {
+    //     // Fetch Discord status every 10 seconds
+    //     const fetchStatus = () => {
+    //         fetch("/api/discord-status")
+    //             .then(res => res.json())
+    //             .then(data => setStatus(data.status))
+    //             .catch(() => setStatus(null));
+    //     };
+    //     fetchStatus();
+    //     const interval = setInterval(fetchStatus, 10000);
+    //     return () => clearInterval(interval);
+    // }, []);
 
     if (error) {
         return <div>Error: {error}</div>;
     }
 
-    let statusString = "Unknown";
-    let statusColor = "status-neutral";
-    let ringColor = "ring-neutral";
-    if (status === "online") {
-        statusString = "Online";
-        statusColor = "status-success";
-        ringColor = "ring-success";
-    } else if (status === "idle") {
-        statusString = "Idle";
-        statusColor = "status-warning";
-        ringColor = "ring-warning";
-    } else if (status === "dnd") {
-        statusString = "Do Not Disturb";
-        statusColor = "status-error";
-        ringColor = "ring-error";
-    } else if (status === "offline" || status === "invisible" || status === null) {
-        statusString = "Offline";
-        statusColor = "status-neutral";
-        ringColor = "ring-neutral";
-    }
+    // let statusString = "Unknown";
+    // let statusColor = "status-neutral";
+    // let ringColor = "ring-neutral";
+    // if (status === "online") {
+    //     statusString = "Online";
+    //     statusColor = "status-success";
+    //     ringColor = "ring-success";
+    // } else if (status === "idle") {
+    //     statusString = "Idle";
+    //     statusColor = "status-warning";
+    //     ringColor = "ring-warning";
+    // } else if (status === "dnd") {
+    //     statusString = "Do Not Disturb";
+    //     statusColor = "status-error";
+    //     ringColor = "ring-error";
+    // } else if (status === "offline" || status === "invisible" || status === null) {
+    //     statusString = "Offline";
+    //     statusColor = "status-neutral";
+    //     ringColor = "ring-neutral";
+    // }
 
     return (
         <Page className="flex flex-col">
@@ -70,29 +71,27 @@ const Profile = ({ user, error }: { user: Discord.APIUser; error: string | null 
                 </div>
                 <div className="flex flex-col justify-center py-10 md:flex-row md:items-center">
                     <div className="flex flex-col justify-center items-center pb-10 md:pb-0">
-                        <div className="tooltip" data-tip="Discord Status">
+                        {/* <div className="tooltip" data-tip="Discord Status">
                             <div className="items-center tooltip-content">
                                 <div className="mr-2 inline-grid *:[grid-area:1/1] ">
                                     <div className={`status ${statusColor} animate-ping`}></div>
                                     <div className={`status ${statusColor}`}></div>
                                 </div>
                                 <span className="mt-2">Currently {statusString} on Discord</span>
-                            </div>
-                            <div className="mt-4 avatar">
-                                <div className={`w-46 h-46 rounded-full ring-5 ring-offset-2 ring-offset-base-100 ${ringColor}`}>
-                                    <Image
-                                        src={
-                                            user?.avatar ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?format=webp&quality=lossless` : "/static/favicon.ico"
-                                        }
-                                        alt="Tiramitzu AKA Syah Warid Ghani Akram"
-                                        height={256}
-                                        width={256}
-                                        className="rounded-full"
-                                        priority
-                                    />
-                                </div>
+                            </div> */}
+                        <div className="mt-4 avatar">
+                            <div className={`w-46 h-46 rounded-full`}>
+                                <Image
+                                    src={user?.avatar ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?format=webp&quality=lossless` : "/static/favicon.ico"}
+                                    alt="Tiramitzu AKA Syah Warid Ghani Akram"
+                                    height={256}
+                                    width={256}
+                                    className="rounded-full"
+                                    priority
+                                />
                             </div>
                         </div>
+                        {/* </div> */}
                     </div>
                     <div className="ml-10 shadow-sm card bg-base-100">
                         <div className="card-body">
